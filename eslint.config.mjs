77e3babad6c52e8d1f93e6a4f1b06b4416992f36
tnghgks <tnghgks@naver.com>
2026-02-21 01:37:28 +0900

    style: add .prettierrc, change eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,12 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ]);
 
 export default eslintConfig;
