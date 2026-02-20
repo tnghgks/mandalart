cd7916360b917da34277121a0812f89ba7c8a215
[OBIGO] Dennis Han <dennis.han@obigo.com>
2026-02-20 18:51:32 +0900

    feat: add Gird props type

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,18 +1,18 @@
 import { cn } from "@/lib/utils";
 
-export const Grid = ({
-  children,
-  className,
-}: {
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
   children?: React.ReactNode;
   className?: string;
-}) => {
+}
+
+export const Grid = ({ children, className, ...rest }: Props) => {
   return (
     <div
       className={cn(
         `relative grid grid-cols-3 md:grid-cols-3 flex-col items-center justify-center rounded-[32px] p-10 shadow-2xl ring-1 ring-white/5`,
         className,
       )}
+      {...rest}
     >
       {children}
     </div>
