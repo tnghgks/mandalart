1ae29ffb08ecc4c703d7ae32e95d26f4197ca8c2
tnghgks <tnghgks@naver.com>
2026-02-21 01:39:19 +0900

    feat: synchronize localstorage to Mandalart

--- src/components/Mandalart.tsx
+++ src/components/Mandalart.tsx
@@ -27,20 +27,29 @@ export default function Mandalart() {
   return (
     <Grid className="w-full max-w-7xl gap-8 overflow-hidden rounded-2xl">
       {Array.from({ length: 9 }).map((_, grid_index) => (
-        <Grid key={grid_index} className="h-full w-full gap-3">
+        <Grid
+          id={`Mandalart-grid-${grid_index}`}
+          key={grid_index}
+          className="h-full w-full gap-3"
+        >
           {Array.from({ length: 9 }).map((_, cell_index) => {
             const globalIndex = grid_index * 9 + cell_index;
 
             return (
-              <textarea
+              <div
                 key={cell_index}
-                value={data[globalIndex]}
-                onChange={(e) => handleUpdate(globalIndex, e.target.value)}
                 className={cn(
-                  "aspect-square resize-none overflow-hidden rounded-xl border border-[#2A2A2F] bg-[#1F1F23] p-3.5 text-sm text-white transition-all duration-200 ease-in outline-none hover:border-[#3A3A40] focus:border-[#7C3AED]",
+                  "flex aspect-square resize-none items-center overflow-hidden rounded-xl border border-[#2A2A2F] bg-[#1F1F23] p-3.5 text-sm text-white transition-all duration-200 ease-in outline-none hover:border-[#3A3A40] focus:border-[#7C3AED]",
                   cell_index === 4 && "border-0 bg-[#7C3AED] font-semibold",
                 )}
-              />
+              >
+                <textarea
+                  id={`Mandalart-cell-${grid_index}-${cell_index}`}
+                  value={data[globalIndex]}
+                  onChange={(e) => handleUpdate(globalIndex, e.target.value)}
+                  className="w-full resize-none overflow-hidden border-0 bg-transparent pt-[40%] pb-[40%] text-center outline-none"
+                />
+              </div>
             );
           })}
         </Grid>
