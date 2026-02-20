4033168083abd2fec2f008a77c06da9300a10dfc
[OBIGO] Dennis Han <dennis.han@obigo.com>
2026-02-20 19:34:09 +0900

    feat: add Mandalart page

---
+++ src/app/mandalart/page.tsx
@@ -0,0 +1,9 @@
+import Mandalart from "@/components/Mandalart";
+
+export default function MandalartPage() {
+  return (
+    <div className="w-full h-screen flex justify-center items-center ">
+      <Mandalart />
+    </div>
+  );
+}
